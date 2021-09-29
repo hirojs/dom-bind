e6ace5d4489de81a9fec0e1529d40b3b768fb3bd
Jason Frame <jwf@jasonframe.co.uk>
2021-09-29 13:16:31 +0100

    updates, make it a bit more modern

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
-var matches = require('dom-matchesselector');
-
-var bind = null, unbind = null;
-
-if (typeof window.addEventListener === 'function') {
-
-	bind = function(el, evtType, cb, useCapture) {
-		el.addEventListener(evtType, cb, useCapture || false);
-		return cb;
-	}
-
-	unbind = function(el, evtType, cb, useCapture) {
-		el.removeEventListener(evtType, cb, useCapture || false);
-		return cb;
-	}
-
-} else if (typeof window.attachEvent === 'function') {
-
-	bind = function(el, evtType, cb, useCapture) {
-		
-		function handler(evt) {
-			evt = evt || window.event;
-			
-			if (!evt.preventDefault) {
-				evt.preventDefault = function() { evt.returnValue = false; }
-			}
-			
-			if (!evt.stopPropagation) {
-				evt.stopPropagation = function() { evt.cancelBubble = true; }
-			}
-
-			cb.call(el, evt);
-		}
-		
-		el.attachEvent('on' + evtType, handler);
-		return handler;
-	
-	}
-
-	unbind = function(el, evtType, cb, useCapture) {
-		el.detachEvent('on' + evtType, cb);
-		return cb;
-	}
-
+const bind = exports.bind = (el, evtType, cb, useCapture) => {
+	el.addEventListener(evtType, cb, useCapture || false);
+	return cb;
 }
 
-function delegate(el, evtType, selector, cb, useCapture) {
+const unbind = exports.unbind = (el, evtType, cb, useCapture) => {
+	el.removeEventListener(evtType, cb, useCapture || false);
+	return cb;
+}
+
+const delegate = exports.delegate = (el, evtType, selector, cb, useCapture) => {
 	return bind(el, evtType, function(evt) {
 		var currTarget = evt.target;
 		while (currTarget && currTarget !== el) {
-			if (matches(selector, currTarget)) {
+			if (currTarget.matches(selector)) {
 				evt.delegateTarget = currTarget;
 				cb.call(el, evt);
 				break;
@@ -58,31 +22,32 @@ function delegate(el, evtType, selector, cb, useCapture) {
 	}, useCapture);
 }
 
-function bind_c(el, evtType, cb, useCapture) {
+const onceOnly = (fn) => {
+	let called = false;
+	return () => {
+		if (called) return;
+		called = true;
+		return fn();
+	}
+};
+
+exports.bind_c = (el, evtType, cb, useCapture) => {
 	cb = bind(el, evtType, cb, useCapture);
-
-	var removed = false;
-	return function() {
-		if (removed) return;
-		removed = true;
+	return onceOnly(() => {
 		unbind(el, evtType, cb, useCapture);
 		el = cb = null;
-	}
+	});
 }
 
-function delegate_c(el, evtType, selector, cb, useCapture) {
+exports.delegate_c = (el, evtType, selector, cb, useCapture) => {
 	cb = delegate(el, evtType, selector, cb, useCapture);
-
-	var removed = false;
-	return function() {
-		if (removed) return;
-		removed = true;
+	return onceOnly(() => {
 		unbind(el, evtType, cb, useCapture);
 		el = cb = null;
-	}
+	});
 }
 
-function once(el, evtType, fn, useCapture) {
+exports.once = (el, evtType, fn, useCapture) => {
 	var called = false;
 	return bind(el, evtType, function _handler(evt) {
 		if (!called) {
@@ -93,7 +58,7 @@ function once(el, evtType, fn, useCapture) {
 	}, useCapture);
 }
 
-function once_c(el, evtType, fn, useCapture) {
+exports.once_c = (el, evtType, fn, useCapture) => {
 	var handler = once(el, evtType, fn, useCapture);
 	var cancelled = false;
 	return function() {
@@ -102,11 +67,3 @@ function once_c(el, evtType, fn, useCapture) {
 		unbind(el, evtType, handler, useCapture);
 	}
 }
-
-exports.bind = bind;
-exports.unbind = unbind;
-exports.delegate = delegate;
-exports.bind_c = bind_c;
-exports.delegate_c = delegate_c;
-exports.once = once;
-exports.once_c = once_c;
